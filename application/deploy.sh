e5f65c06307de5d1ef1fdc48dd3c58137e945208
Achim Krämer <72191461+achim-kraemer@users.noreply.github.com>
2026-02-25 18:48:43 +0100

    main: favicon corrected

--- application/deploy.sh
+++ application/deploy.sh
@@ -4,4 +4,4 @@ composer install
 php bin/console doctrine:migrations:migrate -n
 php bin/console cache:clear
 npm install
-rm -rf public/assets/ && php bin/console asset-map:compile
+rm -rf public/assets/ && php bin/console asset-map:compile --env=prod
